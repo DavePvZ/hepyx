bd66813580a67a4fa85facf13cf99284921db693
DavePvZ <Typolomik953@gmail.com>
2022-08-14 09:50:32 +0700

    added writeability
    (But at the moment it's only for hex section)

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import sys  # sus
 import curses
 import typing
 import string
-# import logging
-# from logging import FileHandler
+import logging
 
 # -----------------------------------------------------
 filename_pos: int = 1
@@ -36,13 +35,20 @@ syms_sep: str = ""
 
 
 def main(sys_argv: list[str]) -> None:
-    # logger = logging.getLogger(__name__)
-    # logger.setLevel(logging.DEBUG)
-    # handler = FileHandler("a thing.log", encoding="utf8")
-    # logger.addHandler(handler)
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler("pyhex_logs.log")
+    file_handler.setLevel(logging.DEBUG)
+    logger_formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s", "%H:%M.%S")
+    file_handler.setFormatter(logger_formatter)
+    logger.addHandler(file_handler)
+    logger.info("Start logging...")
+
     try:
         fname: str = sys_argv[filename_pos]
+        logger.info("Got filename...")
     except IndexError:
+        logger.info("Haven't got filename, exit")
         raise ValueError("not enough arguments.") from None
     perms: tuple[bool, ...] = tuple([access(fname, perm) for perm in (os.F_OK,    # existance
                                                                       os.R_OK,    # readable
@@ -51,11 +57,18 @@ def main(sys_argv: list[str]) -> None:
     # tbh all perms except writeable and executable can be removed
     # bc anyway if below won't let you further
     if not perms[1]:
+        logger.info(f"\"{fname}\" can't be {'read' if perms[0] else 'opened'}, exit")
         raise PermissionError(f"\"{fname}\" can't be {'read' if perms[0] else 'opened'}.")
-    file: typing.BinaryIO = open(fname, mode=f"{'a' if perms[1] and perms[2] else 'r'}+b")
+    file: typing.BinaryIO = open(fname, mode=f"rb{'+' if perms[1] and perms[2] else ''}")
+    logger.info(f"Opened \"{fname}\"")
     stdscr = curses.initscr()
     curses.start_color()
+    # color_pair(1) - inverted colors
+    # color_pair(2) - changed and not selected
+    # color_pair(3) - changed and selected
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_WHITE)
     curses.set_tabsize(4)  # this can be removed bc anyway there's won't be any tabs
     curses.curs_set(0)
     curses.noecho()
@@ -76,23 +89,31 @@ def main(sys_argv: list[str]) -> None:
     file_offset: int = 0  # must be file_offset % 16 == 0
     # and yeah, this is also an up left corner
     file_size: int = os.path.getsize(fname)
-    size_len: int = len(hex(file_size)) - 2
     curr_encoding: str = "ascii"
     changes: dict = {}
+    user_input = 0
     while True:
-        absolute_cursor_pos = sum((file_offset, cursor[0] * 16, cursor[1]))
+        logger.debug(f"\n{cursor=}\n{file_offset=}\n{curr_encoding=}\n{changes=}\n")
         stdscr.attron(curses.color_pair(1))
         stdscr.addstr(0, 0, " " * maxx)
         stdscr.addstr(maxy - maxy_minus, 0, " " * (maxx - 1))
         stdscr.addstr(0, 2, perms_str)
         stdscr.addstr(0, int((maxx-len(fname)) / 2), f"[{fname}]")
+        stdscr.addstr(0, int(maxx-13), "[Modified]" if changes else "")
         stdscr.attroff(curses.color_pair(1))
+        absolute_cursor_pos = sum((file_offset, cursor[0] * 16, cursor[1]))
+
+        # added it here because after any input sign "[Saved]" will be erased by code above
+        # and if i move input-part here, weird bug appears
+        stdscr.addstr(maxy - maxy_minus, int(maxx / 2),
+                      "[Saved]" if user_input == 19 else "",
+                      curses.color_pair(1))
 
         # this must be optimized
         max_len: int = min_addr_num
         hex_start: str = ''
         for offset in range(file_offset, file_offset+(16*(maxy-1)), 16):
-            rjusted_offset: str = (hex(offset))[2:].rjust(size_len, '0')
+            rjusted_offset: str = (hex(offset))[2:].rjust(len(hex(file_size)) - 2, '0')
             max_len: int = len(rjusted_offset) if len(rjusted_offset) > max_len else max_len
         for line, offset in zip(range(1, maxy - 1), range(file_offset, file_offset+(16*(maxy-1)), 16)):
             # help
@@ -106,14 +127,22 @@ def main(sys_argv: list[str]) -> None:
         for line in range(1, maxy - maxy_minus):
             stdscr.move(line, hex_start)
             for block in range(16):
-                curr_byte: bytes = file.read(1)
+                absolute_print_cursor_pos = sum((file_offset, (line-1)*16, block))
+                if absolute_print_cursor_pos not in changes:
+                    curr_byte: bytes = file.read(1)
+                    color = curses.A_NORMAL if len(curr_byte) else curses.A_DIM
+                else:
+                    curr_byte: bytes = changes[absolute_print_cursor_pos]
+                    file.seek(1, 1)
+                    color = curses.color_pair(2)
                 hexed_byte: str = curr_byte.hex()
                 hexed_byte: str = hexed_byte.upper() if caps_hex else hexed_byte
-                stdscr.addstr(hexed_byte if len(curr_byte) else "..",
-                              curses.A_NORMAL if len(hexed_byte) else curses.A_DIM)
+                stdscr.addstr(hexed_byte if len(hexed_byte) else "..",
+                              color)
                 stdscr.addstr(" " * spaces_hex[sum(counter if block in i
                                                    else 0 for counter, i in enumerate(((1, 13), (3, 11), (5, 9), (7,),
                                                                                        (0, 2, 4, 6, 8, 10, 12, 14))))])
+                stdscr.attroff(curses.color_pair(2))
         syms_start: int = sum((hex_start, spaces_between_hex, 45, 3, 2))
 
         for line in range(1, maxy - maxy_minus):
@@ -126,13 +155,19 @@ def main(sys_argv: list[str]) -> None:
                 # i found out that it replaces these symbols with 65533
                 curr_byte: str = curr_byte if curr_byte.isprintable() else "."
                 stdscr.addstr(line, sum((syms_start, block*(len(syms_sep)+1))),
-                              curr_byte if len(curr_byte) else ".", curses.A_NORMAL if len(curr_byte) else curses.A_DIM)
+                              curr_byte if len(curr_byte) else ".",
+                              curses.A_NORMAL if len(curr_byte) else curses.A_DIM)
 
         hex_addr_symbol: str = hex(cursor[1])[2]
         stdscr.addstr(1+cursor[0], hex_start - len(hex_sym_sep),
                       hex_addr_symbol.upper() if caps_hex else hex_addr_symbol)
         file.seek(file_offset + (cursor[0] * 16) + cursor[1], 0)
-        hexed_byte: bytes = file.read(1)
+        if absolute_cursor_pos not in changes:
+            hexed_byte: bytes = file.read(1)
+            color = curses.color_pair(1)
+        else:
+            hexed_byte: bytes = changes[absolute_cursor_pos]
+            color = curses.color_pair(3)
         if not cursor[2]:
             hexed_byte = hexed_byte.hex()
             hexed_byte = hexed_byte.upper() if caps_hex else hexed_byte
@@ -140,13 +175,14 @@ def main(sys_argv: list[str]) -> None:
                                             sum(spaces_hex[sum(counter if block in i
                                                 else 0 for counter, i in enumerate(((1, 13), (3, 11), (5, 9), (7,),
                                                                                     (0, 2, 4, 6, 8, 10, 12, 14))))]
-                                                for block in range(0, cursor[1])))),
-                          hexed_byte if len(hexed_byte) else "..", curses.color_pair(1))
+                                                for block in range(cursor[1])))),
+                          hexed_byte if len(hexed_byte) else "..", color)
         else:
             hexed_byte = hexed_byte.decode(curr_encoding, "replace")
             hexed_byte: str = hexed_byte if hexed_byte.isprintable() else "."
             stdscr.addstr(cursor[0]+1, sum((syms_start, cursor[1])),
-                          hexed_byte if len(hexed_byte) else ".", curses.color_pair(1))
+                          hexed_byte if len(hexed_byte) else ".", color)
+        stdscr.attroff(curses.color_pair(3))
 
         stdscr.refresh()
 
@@ -154,8 +190,6 @@ def main(sys_argv: list[str]) -> None:
         match user_input:
             case curses.KEY_DOWN:
                 if cursor[3]:
-                    changes[absolute_cursor_pos] = int(changes[absolute_cursor_pos] + "0",
-                                                       16).to_bytes(1, byteorder="big")
                     cursor[3] = 0
                 if cursor[0] < maxy - 3:
                     cursor[0] += 1
@@ -163,8 +197,6 @@ def main(sys_argv: list[str]) -> None:
                     file_offset += 16
             case curses.KEY_UP:
                 if cursor[3]:
-                    changes[absolute_cursor_pos] = int(changes[absolute_cursor_pos] + "0",
-                                                       16).to_bytes(1, byteorder="big")
                     cursor[3] = 0
                 if cursor[0] > 0:
                     cursor[0] -= 1
@@ -172,8 +204,6 @@ def main(sys_argv: list[str]) -> None:
                     file_offset -= 16
             case curses.KEY_RIGHT:
                 if cursor[3]:
-                    changes[absolute_cursor_pos] = int(changes[absolute_cursor_pos] + "0",
-                                                       16).to_bytes(1, byteorder="big")
                     cursor[3] = 0
                 if cursor[1] < 15:
                     cursor[1] += 1
@@ -182,8 +212,6 @@ def main(sys_argv: list[str]) -> None:
                     cursor[1] = 0
             case curses.KEY_LEFT:
                 if cursor[3]:
-                    changes[absolute_cursor_pos] = int(changes[absolute_cursor_pos] + "0",
-                                                       16).to_bytes(1, byteorder="big")
                     cursor[3] = 0
                 if cursor[1] > 0:
                     cursor[1] -= 1
@@ -193,29 +221,69 @@ def main(sys_argv: list[str]) -> None:
             # Ctrl+X - quit (and maybe save)
             case 24:  # ord(Ctrl+X)
                 if len(changes):
-                    for address, value in changes.items():
-                        file.seek(address, 0)
-                        file.write(value)
-                break
+                    choice = 0
+                    """
+                    +------------------------------------------+
+                    | Do you want to save changes before exit? |
+                    |                                          |
+                    |       [Edit]      [No]      [Yes]        |
+                    +------------------------------------------+
+                    """
+                    stdscr.addstr(int(maxy/2)-2, int((maxx-45)/2), f"+{'-' * 42}+")
+                    stdscr.addstr(int(maxy/2)-1, int((maxx-45)/2), "| Do you want to save changes before exit? |")
+                    stdscr.addstr(int(maxy/2), int((maxx-45)/2), f"|{' ' * 42}|")
+                    stdscr.addstr(int(maxy/2)+1, int((maxx-45)/2), "|       [Edit]      [No]      [Yes]        |")
+                    stdscr.addstr(int(maxy/2)+2, int((maxx-45)/2), f"+{'-' * 42}+")
+                    while True:
+                        stdscr.addstr(int(maxy / 2) + 1, int(maxx / 2) - 16,
+                                      "[Edit]".rjust(7, ">" if not choice
+                                                     else " ").ljust(8, "<" if not choice else " "))
+                        stdscr.addstr(int(maxy / 2) + 1, int(maxx / 2) - 4,
+                                      "[No]".rjust(5, ">" if choice == 1
+                                                   else " ").ljust(6, "<" if choice == 1 else " "))
+                        stdscr.addstr(int(maxy / 2) + 1, int(maxx / 2) + 6,
+                                      "[Yes]".rjust(6, ">" if choice == 2
+                                                    else " ").ljust(7, "<" if choice == 2 else " "))
+                        user_inp = stdscr.getch()
+                        match user_inp:
+                            case curses.KEY_RIGHT:
+                                choice += 1 if choice < 2 else 0
+                            case curses.KEY_LEFT:
+                                choice -= 1 if choice > 0 else 0
+                            case 10:  # ord(Enter)
+                                break
+                    if choice == 1:
+                        break
+                    elif choice == 2:
+                        for address, value in changes.items():
+                            file.seek(address, 0)
+                            file.write(value)
+                        break
+                else:
+                    break
             # Ctrl+S - save
             case 19:  # ord(Ctrl+S)
+                logger.debug("user pressed Ctrl+S...")
                 changes_copy: dict = changes.copy()
                 for address, value in changes.items():
                     file.seek(address, 0)
                     file.write(value)
                     changes_copy.pop(address)
+                    logger.debug(f"Saved {value} on {address}")
                 changes = changes_copy.copy()
                 del changes_copy
             case _:
                 pass
 
         if chr(user_input) in string.hexdigits:
+            user_input = chr(user_input)
             if absolute_cursor_pos in changes:
-                changes[absolute_cursor_pos] = int(changes[absolute_cursor_pos] + user_input,
-                                                   16).to_bytes(1, byteorder="big")
+                changes[absolute_cursor_pos] = (int.from_bytes(changes[absolute_cursor_pos],
+                                                               byteorder="big", signed=False) +
+                                                int(user_input, 16)).to_bytes(1, byteorder="big")
                 cursor[3] = 0
             else:
-                changes[absolute_cursor_pos] = user_input
+                changes[absolute_cursor_pos] = (int(user_input, 16) * 16).to_bytes(1, byteorder="big")
                 cursor[3] = 1
     curses.endwin()
     file.close()
